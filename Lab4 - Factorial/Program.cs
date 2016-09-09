cf171109fb5eb72ff3673cd62de6259a6482cff1
bmike0001 <bmike0001@gmail.com>
2016-09-09 15:03:06 -0400

    Add project files.

---
+++ Lab4 - Factorial/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4___Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Factorial Calculator \n");
+
+            { 
+            //loop starts here
+            Console.WriteLine("Enter an integer that's greater than 0 but less than 10: (user input here.)");
+            //reads information form the consoles and convert to a 
+            //number to be use by the program
+            
+            string inputFactorial = Console.ReadLine();
+            int factorialNumber = int.Parse(inputFactorial);
+
+            //calculate a factorial
+
+            int number = factorialNumber;
+            for (int i = factorialNumber - 1; i > 0; i--) 
+            {
+                number = number * i;
+
+            }
+
+
+
+            Console.WriteLine(number );
+          
+
+
+            Console.WriteLine("Continue (y/n)");
+                //end of the loop here.  
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+}
